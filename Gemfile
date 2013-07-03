f79c6bd97eebfa843ac8c30317aa008a19b1005e
Harini Saladi <harini@gurupanguji.com>
2013-07-03 14:22:48 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,30 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0.rc1'
+
+# Use sqlite3 as the database for Active Record
+group :development do
+  gem 'sqlite3'
+  gem 'rspec-rails', '2.11.0'
+end
+
+group :production do
+  gem 'pg'
+end
+
+group :assets do
+  # Use SCSS for stylesheets
+  gem 'sass-rails', '~> 4.0.0.rc1'
+  # Use Uglifier as compressor for JavaScript assets
+  gem 'uglifier', '>= 1.3.0'
+  # Use CoffeeScript for .js.coffee assets and views
+  gem 'coffee-rails', '~> 4.0.0'
+end
+
+group :test do
+  gem 'capybara', '1.1.2'
+end
+
+# Use jquery as the JavaScript library
+gem 'jquery-rails'
+gem 'turbolinks'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MiniTwitter::Application.config.secret_key_base = '20bb27e8c57bfabcc69475045bbe8d1ba125a52b52dd1a2dfba76b1d92d9eb0d967f5e69ab17410edf5e9fa57d4cb4787237ad6c835221500932da66f1ebad30'
